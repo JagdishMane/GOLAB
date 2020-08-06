650517266e08cf7a11e5d77bdfaae9c41e40340c
JM <jagdish.m33@gmail.com>
2020-08-06 22:03:23 +0800

    add files

--- 012-UPDATED-VERSION/004-exercise-1/q3.go
+++ 012-UPDATED-VERSION/004-exercise-1/q3.go
@@ -2,13 +2,11 @@ package main
 
 import "fmt"
 
-var x int = 42
+var x int = 43
 var y string = "James Bond"
 var z bool = true
 
 func main() {
-	fmt.Println(x, y, z)
-	fmt.Println(x)
-	fmt.Println(y)
-	fmt.Println(z)
+	s := fmt.Sprintf("%v\t%s\t%t\t", x, y, z)
+	fmt.Println(s)
 }
